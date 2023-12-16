23e29869501b4341c2d4efd347b14ed261b13d17
RyanGallagher <ryan.l.p.gallagher@icloud.com>
2023-12-16 21:17:29 +0000

    test: added yellow colour to vc so I can see it working

---
+++ tip-calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tip-calculator
+//
+//  Created by Ryan Gallagher on 16/12/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+    }
+
+
+}
+
