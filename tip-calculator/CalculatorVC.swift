d1b3611977beec5aec3acf7debd75ecc8cf1cac9
RyanGallagher <ryan.l.p.gallagher@icloud.com>
2023-12-17 16:40:00 +0000

    calcvc: added vstack for the subviews

--- tip-calculator/CalculatorVC.swift
+++ tip-calculator/CalculatorVC.swift
@@ -14,6 +14,19 @@ class CalculatorVC: UIViewController {
     private let billInputView = BillInputView()
     private let tipInputView = TipInputView()
     private let splitInputView = SplitInputView()
+    
+    private lazy var vStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [
+            logoView,
+            resultView,
+            billInputView,
+            tipInputView,
+            splitInputView
+        ])
+        stackView.axis = .vertical
+        stackView.spacing = 36
+        return stackView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
