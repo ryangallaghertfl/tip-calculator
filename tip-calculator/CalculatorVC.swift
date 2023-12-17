564fd4bfd51aa241135b7241407462f399c3360e
RyanGallagher <ryan.l.p.gallagher@icloud.com>
2023-12-17 16:18:38 +0000

    calcVC: added fields for subviews to be created

--- tip-calculator/CalculatorVC.swift
+++ tip-calculator/CalculatorVC.swift
@@ -8,6 +8,12 @@
 import UIKit
 
 class CalculatorVC: UIViewController {
+    
+    private let logoView = LogoView()
+    private let resultView = ResultView()
+    private let billInputView = BillInputView()
+    private let tipInputView = TipInputView()
+    private let splitInputView = SplitInputView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
