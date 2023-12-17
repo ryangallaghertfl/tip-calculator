ee6ae2d50824e13062fd8c06cbc069565363a413
RyanGallagher <ryan.l.p.gallagher@icloud.com>
2023-12-17 16:30:56 +0000

    added billinputview

---
+++ tip-calculator/View/BillInputView.swift
@@ -0,0 +1,24 @@
+//
+//  BillInputView.swift
+//  tip-calculator
+//
+//  Created by Ryan Gallagher on 17/12/2023.
+//
+
+import UIKit
+
+class BillInputView: UIView {
+    
+    init() {
+        super.init(frame: .zero)
+        layout()
+    }
+    
+    private func layout() {
+        backgroundColor = .green
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
