8356b05555a7a9aaa1710f6acca372de254af22f
RyanGallagher <ryan.l.p.gallagher@icloud.com>
2023-12-17 16:52:54 +0000

    calcvc: constrained heights of subviews

--- tip-calculator/CalculatorVC.swift
+++ tip-calculator/CalculatorVC.swift
@@ -44,6 +44,26 @@ class CalculatorVC: UIViewController {
             make.bottom.equalTo(view.snp.bottomMargin).offset(-16)
             make.top.equalTo(view.snp.topMargin).offset(16)
         }
+        
+        logoView.snp.makeConstraints { make in
+            make.height.equalTo(48)
+        }
+        
+        resultView.snp.makeConstraints { make in
+            make.height.equalTo(224)
+        }
+        
+        billInputView.snp.makeConstraints { make in
+            make.height.equalTo(56)
+        }
+        
+        tipInputView.snp.makeConstraints { make in
+            make.height.equalTo(56+56+16)
+        }
+        
+        splitInputView.snp.makeConstraints { make in
+            make.height.equalTo(56)
+        }
     }
 
 }
