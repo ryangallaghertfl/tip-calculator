7367eb471808b2bf371267b14053f91323d1cbf4
RyanGallagher <ryan.l.p.gallagher@icloud.com>
2023-12-17 17:30:21 +0000

    ThemeColour: added struct and hex values for the theme

--- tip-calculator/config/ThemeColour.swift
+++ tip-calculator/config/ThemeColour.swift
@@ -5,4 +5,12 @@
 //  Created by Ryan Gallagher on 17/12/2023.
 //
 
-import Foundation
+import UIKit
+
+struct ThemeColour {
+    static let bg = UIColor(hexString: "F5F3F4")
+    static let primary = UIColor(hexString: "1CC9BE")
+    static let secondary = UIColor.systemOrange
+    static let text = UIColor(hexString: "000000")
+    static let separator = UIColor(hexString: "CCCCCC")
+}
