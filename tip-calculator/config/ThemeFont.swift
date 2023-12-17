709e553d8da1f9ebc188e8c144ef581ed0f2e4f0
RyanGallagher <ryan.l.p.gallagher@icloud.com>
2023-12-17 17:41:02 +0000

    added ThemeFont struct and methods for theme fonts

--- tip-calculator/config/ThemeFont.swift
+++ tip-calculator/config/ThemeFont.swift
@@ -5,4 +5,20 @@
 //  Created by Ryan Gallagher on 17/12/2023.
 //
 
-import Foundation
+// source from developer.apple.com/fonts/system-fonts/
+import UIKit
+
+struct ThemeFont {
+    //avenir next
+    static func regular(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: "AvenirNext-Regular", size: size) ?? .systemFont(ofSize: size)
+    }
+    
+    static func bold(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: "AvenirNext-Bold", size: size) ?? .systemFont(ofSize: size)
+    }
+    
+    static func demibold(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: "AvenirNext-DemiBold", size: size) ?? .systemFont(ofSize: size)
+    }
+}
