4c582938e55d93b131e63195adf4fe896f7a0e68
RyanGallagher <ryan.l.p.gallagher@icloud.com>
2023-12-18 13:47:15 +0000

    amountview: added amountlabel with mutable attributed string

--- tip-calculator/View/AmountView.swift
+++ tip-calculator/View/AmountView.swift
@@ -10,10 +10,26 @@ import UIKit
 class AmountView: UIView {
     
     private let titleLabel: UILabel = {
-        LabelFactory.build(text: "total bill",
-                           font: ThemeFont.regular(ofSize: 18),
-                           textColour: ThemeColour.text,
-                           textAlignment: .left)
+        LabelFactory.build(
+            text: "total bill",
+            font: ThemeFont.regular(ofSize: 18),
+            textColour: ThemeColour.text,
+            textAlignment: .left)
+    }()
+    
+    private let amountLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .left
+        label.textColor = ThemeColour.primary
+        let text = NSMutableAttributedString(
+            string: "$0",
+            attributes: [
+                .font: ThemeFont.bold(ofSize: 24)
+            ])
+            text.addAttributes([
+                .font: ThemeFont.bold(ofSize: 24)], range: NSMakeRange(0, 1))
+            label.attributedText = text
+        return label
     }()
     
     override init(frame: CGRect) {
