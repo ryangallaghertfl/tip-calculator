2a9f7283f7432a1bfe1fea597022351039b6280f
RyanGallagher <ryan.l.p.gallagher@icloud.com>
2023-12-19 14:10:11 +0000

    TipInputView: added tip enum to switch over button options

--- tip-calculator/View/TipInputView.swift
+++ tip-calculator/View/TipInputView.swift
@@ -26,7 +26,33 @@ class TipInputView: UIView {
         backgroundColor = .systemPink
     }
     
+    
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+}
+
+enum Tip {
+    case none
+    case tenPercent
+    case fifteenPercent
+    case twentyPercent
+    case custom(value: Int)
+    
+    var stringValue: String {
+        switch self {
+        
+        case .none:
+            return ""
+        case .tenPercent:
+            return "10%"
+        case .fifteenPercent:
+            return "15%"
+        case .twentyPercent:
+            return "20%"
+        case .custom(value: let value):
+            return String(value)
+        }
     }
 }
