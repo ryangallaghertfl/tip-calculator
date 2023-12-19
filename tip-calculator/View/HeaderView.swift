e1a586912d3c0ec8aaa817ed5cbb71b9daa8ca14
RyanGallagher <ryan.l.p.gallagher@icloud.com>
2023-12-19 10:42:46 +0000

    HeaderView: added configure method top set text for top and bottom labels

--- tip-calculator/View/HeaderView.swift
+++ tip-calculator/View/HeaderView.swift
@@ -52,6 +52,11 @@ class HeaderView: UIView {
         }
     }
     
+    func configure(topText: String, bottomText: String) {
+        topLabel.text = topText
+        bottomLabel.text = bottomText
+    }
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
