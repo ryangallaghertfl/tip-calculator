3bd3ee8bd6d7c8755b46904e9c74b54c92173e97
RyanGallagher <ryan.l.p.gallagher@icloud.com>
2023-12-19 10:45:58 +0000

    BillInputView: added configuration for headerview

--- tip-calculator/View/BillInputView.swift
+++ tip-calculator/View/BillInputView.swift
@@ -10,7 +10,12 @@ import UIKit
 class BillInputView: UIView {
     
     private let headerView: HeaderView = {
-        return HeaderView()
+        let view = HeaderView()
+        view.configure(
+            topText: "Enter",
+            bottomText: "your bill"
+        )
+        return view
     }()
     
     private let textFieldContainerView: UIView = {
