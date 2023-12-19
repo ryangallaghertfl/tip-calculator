a7cdf5e4f34de3c70680808a703b858a2f26610a
RyanGallagher <ryan.l.p.gallagher@icloud.com>
2023-12-19 17:19:02 +0000

    CalculatorVM: added struct with ref to publishers from inputs from vc

--- tip-calculator/ViewModel/CalculatorVM.swift
+++ tip-calculator/ViewModel/CalculatorVM.swift
@@ -6,7 +6,17 @@
 //
 
 import Foundation
+import Combine
 
 class CalculatorVM {
     
+    struct InputFromVC {
+        let billPublisher: AnyPublisher<Double, Never>
+        let tipPublisher: AnyPublisher<Tip, Never>
+        let splitPublisher: AnyPublisher<Int, Never>
+    }
+    
+    struct OutputToView {
+        
+    }
 }
