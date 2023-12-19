2a94923a0ef6f86f595b32aad5f130006dd253e9
RyanGallagher <ryan.l.p.gallagher@icloud.com>
2023-12-19 17:21:10 +0000

    added Result model

---
+++ tip-calculator/Models/Result.swift
@@ -0,0 +1,14 @@
+//
+//  Result.swift
+//  tip-calculator
+//
+//  Created by Ryan Gallagher on 19/12/2023.
+//
+
+import Foundation
+
+struct Result {
+    let amountPerPerson: Double
+    let totalBill: Double
+    let totalTill: Double
+}
