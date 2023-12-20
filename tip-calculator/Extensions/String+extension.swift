7a03760b0a3432f403ef71e46c0bf91675552875
RyanGallagher <ryan.l.p.gallagher@icloud.com>
2023-12-20 12:05:20 +0000

    added string extension to cast to double

---
+++ tip-calculator/Extensions/String+extension.swift
@@ -0,0 +1,14 @@
+//
+//  String+extension.swift
+//  tip-calculator
+//
+//  Created by Ryan Gallagher on 20/12/2023.
+//
+
+import Foundation
+
+extension String {
+    var doubleValue: Double? {
+        Double(self)
+    }
+}
