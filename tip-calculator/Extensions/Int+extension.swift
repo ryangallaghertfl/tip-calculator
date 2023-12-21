b3b73c3dda11b6517c102fcd9ebd7f773ac58a77
RyanGallagher <ryan.l.p.gallagher@icloud.com>
2023-12-21 14:55:16 +0000

    added Int ext which casts to string

---
+++ tip-calculator/Extensions/Int+extension.swift
@@ -0,0 +1,14 @@
+//
+//  Int+extension.swift
+//  tip-calculator
+//
+//  Created by Ryan Gallagher on 21/12/2023.
+//
+
+import Foundation
+
+extension Int {
+    var stringValue: String {
+        return String(self)
+    }
+}
