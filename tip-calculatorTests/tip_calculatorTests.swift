9d0d6fe89b57a2d7c9050032c07893a3bf68cc6e
RyanGallagher <ryan.l.p.gallagher@icloud.com>
2023-12-22 17:33:26 +0000

    test: added fakeInputFromVC func, tested 1 person, 100 amount, no tip and asserted across total tip, total bill and amount per person

--- tip-calculatorTests/tip_calculatorTests.swift
+++ tip-calculatorTests/tip_calculatorTests.swift
@@ -13,6 +13,8 @@ final class tip_calculatorTests: XCTestCase {
     
     private var sut: CalculatorVM!
     private var cancellables: Set<AnyCancellable>!
+    
+    private let logoViewTapSubject = PassthroughSubject<Void, Never>()
 
     override func setUp() {
         sut = .init()
@@ -25,5 +27,30 @@ final class tip_calculatorTests: XCTestCase {
         sut = nil
         cancellables = nil
     }
+    
+    func test_Result_givenNoTip_1Person_amount100_totalPerPerson_shouldBe100() {
+        
+        let bill: Double = 100.00
+        let tip: Tip = .none
+        let split: Int = 1
+        let mockInput = fakeBuildInput(bill: bill, tip: tip, split: split)
+        
+        let vmOutput = sut.transform(inputFromVC: mockInput)
+        
+        vmOutput.updateViewPublisher.sink { result in
+            XCTAssertEqual(result.amountPerPerson, 100)
+            XCTAssertEqual(result.totalBill, 100)
+            XCTAssertEqual(result.totalTip, 0)
+        }.store(in: &cancellables)
+    }
 
+        //mocks the input from a VC
+    private func fakeBuildInput(bill: Double, tip: Tip, split: Int) -> CalculatorVM.InputFromVC {
+        return .init(
+            billPublisher: Just(bill).eraseToAnyPublisher(),
+            tipPublisher: Just(tip).eraseToAnyPublisher(),
+            splitPublisher: Just(split).eraseToAnyPublisher(),
+            logoViewTapPublisher: logoViewTapSubject.eraseToAnyPublisher()
+        )
+    }
 }
