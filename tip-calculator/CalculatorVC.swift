e6159d0c3d61756c5620cf14a0e2625703388852
RyanGallagher <ryan.l.p.gallagher@icloud.com>
2023-12-22 17:02:05 +0000

    CalculatorVC: bind - resetCalculatorPublisher.sinl now animates the logo and returns it to original positon

--- tip-calculator/CalculatorVC.swift
+++ tip-calculator/CalculatorVC.swift
@@ -78,8 +78,21 @@ class CalculatorVC: UIViewController {
             billInputView.reset()
             tipInputView.reset()
             splitInputView.reset()
+            
+            UIView.animate(
+                withDuration: 0.1,
+                delay: 0,
+                usingSpringWithDamping: 5.0,
+                initialSpringVelocity: 0.5,
+                options: .curveEaseInOut) {
+                //expand the logoView
+                self.logoView.transform = .init(scaleX: 1.5, y: 1.5)
+            } completion: { _ in
+                UIView.animate(withDuration: 0.1) { //returns logoview to original position
+                    self.logoView.transform = .identity
+                }
+            }
         }.store(in: &cancellables)
-        
     }
     
     private func observe() {
