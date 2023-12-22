ac9dd9f72e5a9ec22463a1bbd20dcce4f7129762
RyanGallagher <ryan.l.p.gallagher@icloud.com>
2023-12-22 15:45:06 +0000

    added AudioPlayerService protocol and DefaultAudioPlayer implementation scaffolding

---
+++ tip-calculator/Services/AudioPlayerService.swift
@@ -0,0 +1,22 @@
+//
+//  AudioPlayerService.swift
+//  tip-calculator
+//
+//  Created by Ryan Gallagher on 22/12/2023.
+//
+
+import Foundation
+import AVFoundation
+
+protocol AudioPlayerService {
+    func playSound()
+}
+
+final class DefaultAudioPlayer: AudioPlayerService {
+    
+    func playSound() {
+        <#code#>
+    }
+    
+    
+}
