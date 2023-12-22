0bb2445d55b2ed6e9c354228517f0f50d8ff6584
RyanGallagher <ryan.l.p.gallagher@icloud.com>
2023-12-22 15:51:17 +0000

    DefaultAudioPlayer: playSound - finds path of sound file, passes in to AVAudioPlayer and attempts to call play(), catch block will print errors

--- tip-calculator/Services/AudioPlayerService.swift
+++ tip-calculator/Services/AudioPlayerService.swift
@@ -17,8 +17,13 @@ final class DefaultAudioPlayer: AudioPlayerService {
     private var player: AVAudioPlayer?
     
     func playSound() {
-        <#code#>
+        let path = Bundle.main.path(forResource: "click", ofType: "m4a")!
+        let url = URL(fileURLWithPath: path)
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.play()
+        } catch(let error) {
+            print(error.localizedDescription)
+        }
     }
-    
-    
 }
