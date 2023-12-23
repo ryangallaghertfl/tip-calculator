d876ff1be426540809db0ecc81f8381b145cfd26
RyanGallagher <ryan.l.p.gallagher@icloud.com>
2023-12-23 16:47:50 +0000

    test: added snapshot testing file and set up variable

---
+++ tip-calculatorTests/tip_calculatorSnapshotTests.swift
@@ -0,0 +1,17 @@
+//
+//  tip_calculatorSnapshotTests.swift
+//  tip-calculatorTests
+//
+//  Created by Ryan Gallagher on 23/12/2023.
+//
+
+import XCTest
+import SnapshotTesting
+@testable import tip_calculator
+
+final class tip_calculatorSnapshotTests: XCTestCase {
+    
+    private var screenWidth: CGFloat {
+        return UIScreen.main.bounds.size.width
+    }
+}
