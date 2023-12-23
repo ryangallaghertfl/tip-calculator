3be3970efaa5b86c37f8fb08d816b6c797e2e440
RyanGallagher <ryan.l.p.gallagher@icloud.com>
2023-12-23 17:12:28 +0000

    snapshot test: asserted splitInputView

--- tip-calculatorTests/tip_calculatorSnapshotTests.swift
+++ tip-calculatorTests/tip_calculatorSnapshotTests.swift
@@ -63,7 +63,7 @@ final class tip_calculatorSnapshotTests: XCTestCase {
         
         let view = SplitInputView()
         
-        assertSnapshot(matching: view, as: .image(size: size), record: true)
-        //assertSnapshot(matching: view, as: .image(size: size))
+        //assertSnapshot(matching: view, as: .image(size: size), record: true)
+        assertSnapshot(matching: view, as: .image(size: size))
     }
 }
