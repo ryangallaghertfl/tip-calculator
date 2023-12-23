c810cc324fc365dafcaa160f00295fcc0cc0eb4e
RyanGallagher <ryan.l.p.gallagher@icloud.com>
2023-12-23 17:01:07 +0000

    snapshot test: added test for logoView and recorded image for future test runs

--- tip-calculatorTests/tip_calculatorSnapshotTests.swift
+++ tip-calculatorTests/tip_calculatorSnapshotTests.swift
@@ -14,4 +14,16 @@ final class tip_calculatorSnapshotTests: XCTestCase {
     private var screenWidth: CGFloat {
         return UIScreen.main.bounds.size.width
     }
+    
+    func test_logoView() {
+        
+        let size = CGSize(width: screenWidth, height: 48)
+        
+        let view = LogoView()
+        
+        //choose assertSnapshort, not assertSnapshots
+        //record the snapshot once and then comment out record
+        //assertSnapshot(matching: view, as: .image(size: size), record: true)
+        assertSnapshot(matching: view, as: .image(size: size))
+    }
 }
