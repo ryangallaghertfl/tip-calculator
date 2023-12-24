3a66b979d691f5b5f46513a8f114a2aed17e4517
RyanGallagher <ryan.l.p.gallagher@icloud.com>
2023-12-24 14:48:10 +0000

    UIView ext: added function to get subViews of a particular type from view recursively. It would look recursively in all subviews and return back the subviews of the type T

--- tip-calculatorTests/tip_calculatorSnapshotTests.swift
+++ tip-calculatorTests/tip_calculatorSnapshotTests.swift
@@ -79,3 +79,36 @@ final class tip_calculatorSnapshotTests: XCTestCase {
         assertSnapshot(matching: view, as: .image(size: size))
     }
 }
+
+//this will allow us to access all subviews of the UIView type, which helps me examine the textfields inside of BillInputView
+
+extension UIView {
+    //        - https://stackoverflow.com/a/45297466/6181721
+
+    /** This is the function to get subViews of a view of a particular type
+*/
+    func subViews<T : UIView>(type : T.Type) -> [T]{
+        var all = [T]()
+        for view in self.subviews {
+            if let aView = view as? T{
+                all.append(aView)
+            }
+        }
+        return all
+    }
+
+
+/** This is a function to get subViews of a particular type from view recursively. It would look recursively in all subviews and return back the subviews of the type T */
+        func allSubViewsOf<T : UIView>(type : T.Type) -> [T]{
+            var all = [T]()
+            func getSubview(view: UIView) {
+                if let aView = view as? T{
+                all.append(aView)
+                }
+                guard view.subviews.count>0 else { return }
+                view.subviews.forEach{ getSubview(view: $0) }
+            }
+            getSubview(view: self)
+            return all
+        }
+    }
