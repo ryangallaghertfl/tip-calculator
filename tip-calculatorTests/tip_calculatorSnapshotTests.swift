f0ba2e9e66d370e6f02884108697d7c2b05c41d4
RyanGallagher <ryan.l.p.gallagher@icloud.com>
2023-12-24 15:04:51 +0000

    test: asserted snapshot test for splitInputView for when number of people is incremented

--- tip-calculatorTests/tip_calculatorSnapshotTests.swift
+++ tip-calculatorTests/tip_calculatorSnapshotTests.swift
@@ -116,8 +116,8 @@ final class tip_calculatorSnapshotTests: XCTestCase {
         //taps which should sendAction to increment number of people to 2
         button?.sendActions(for: .touchUpInside)
         
-        assertSnapshot(matching: view, as: .image(size: size), record: true)
-        //assertSnapshot(matching: view, as: .image(size: size))
+        //assertSnapshot(matching: view, as: .image(size: size), record: true)
+        assertSnapshot(matching: view, as: .image(size: size))
     }
 }
 
