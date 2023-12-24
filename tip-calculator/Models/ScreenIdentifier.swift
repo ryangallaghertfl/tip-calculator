6cfa98c2a457a6020a096467009654957758e301
RyanGallagher <ryan.l.p.gallagher@icloud.com>
2023-12-24 15:30:04 +0000

    test: to set up UI testing, added ScreenIdentifier enum

---
+++ tip-calculator/Models/ScreenIdentifier.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenIdentifier.swift
+//  tip-calculator
+//
+//  Created by Ryan Gallagher on 24/12/2023.
+//
+
+import Foundation
+
+enum ScreenIdentifier {
+     
+    enum ResultView: String {
+        case totalAmountPerPersonValueLabel
+        case totalBillValueLabel
+        case totalTipValueLabel
+    }
+}
