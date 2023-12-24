083736c43864d064a771dc04f24c77347f37c9d3
RyanGallagher <ryan.l.p.gallagher@icloud.com>
2023-12-24 15:53:48 +0000

    Calculator UI tests: finished setup, passed in CalculatorScreen with setup and tearDown methods

--- tip-calculatorUITests/tip_calculatorUITests.swift
+++ tip-calculatorUITests/tip_calculatorUITests.swift
@@ -11,6 +11,21 @@ final class tip_calculatorUITests: XCTestCase {
 
     private var app: XCUIApplication!
     
+    private var screen: CalculatorScreen {
+        CalculatorScreen(app: app)
+    }
+    
+    override func setUp() {
+        super.setUp()
+        app = .init()
+        app.launch()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        app = nil
+    }
+    
     func test_ResultViewDefaultValues_ShouldBeZero() {
         
     }
