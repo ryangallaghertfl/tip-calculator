fdddf864ae256a82fe2374932cc2f1c2e46a084f
RyanGallagher <ryan.l.p.gallagher@icloud.com>
2023-12-24 15:48:30 +0000

    test: UI setup, added CalculatorScreen class which return screenIdentifier labels, added totalAmountPerPerson

---
+++ tip-calculatorUITests/CalculatorScreen.swift
@@ -0,0 +1,22 @@
+//
+//  CalculatorScreen.swift
+//  tip-calculatorUITests
+//
+//  Created by Ryan Gallagher on 24/12/2023.
+//
+
+import XCTest
+
+class CalculatorScreen {
+    
+    private let app: XCUIApplication
+    
+    init(app: XCUIApplication) {
+        self.app = app
+    }
+    
+    var totalAmountPerPersonValueLabel: XCUIElement {
+        return app.staticTexts[ScreenIdentifier.ResultView.totalAmountPerPersonValueLabel.rawValue]
+    }
+}
+
